2554dcc150bd76228278d21c6478ea09c1bdf3c8
Juho Teperi <juho.teperi@iki.fi>
2015-04-09 15:38:26 +0300

    Min & max date

--- example/src/cljs/example/reagent.cljs
+++ example/src/cljs/example/reagent.cljs
@@ -1,7 +1,9 @@
 (ns ^:figwheel-always example.reagent
   (:require [reagent.core :as reagent :refer [atom]]
-            [reagent.ratom :refer-macros [reaction]]
+            [reagent.ratom :refer-macros [reaction run!]]
+            [schema.coerce :as sc]
             [cljs-time.core :as t]
+            [lomakkeet.core :as l]
             [lomakkeet.reagent :as f]
             [example.domain :as d]
             [example.reagent.autocomplete :as eac]
@@ -10,52 +12,52 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:example-page (f/->fs d/empty-thing d/Thingie)}))
+(defonce app-state (atom {:example-page (-> (f/->fs d/empty-thing d/Thingie))}))
 
 (defn thing-view []
-  (let [form (reagent/cursor app-state [:example-page])]
+  (let [form  (reagent/cursor app-state [:example-page])
+        start (reaction (get-in (::l/value @form) [:dates :start]))
+        end   (reaction (get-in (::l/value @form) [:dates :end]))]
     (fn []
-      (let [{{:keys [start end]} :dates} @form]
-        [:div.tasks
-         [:h2
-          "Basic fields"
-          [:div.btn-toolbar.pull-right
-           [forms/form-status form]
-           [forms/cancel-btn form]
-           [forms/save-btn form]]]
+      [:div.tasks
+       [:h2
+        "Basic fields"
+        [:div.btn-toolbar.pull-right
+         [forms/form-status form]
+         [forms/cancel-btn form]
+         [forms/save-btn form]]]
 
-         [:form.column-content
-          [:div.row
-           [f/input form "Name"   [:name]]
-           [f/input form "Email"  [:email]]]
+       [:form.column-content
+        [:div.row
+         [f/input form "Name"   [:name]]
+         [f/input form "Email"  [:email]]]
 
-          [:div.row
-           [f/textarea  form "Textarea" [:desc]]
-           [f/select    form "Select"   [:gender] d/genders]]
+        [:div.row
+         [f/textarea  form "Textarea" [:desc]]
+         [f/select    form "Select"   [:gender] d/genders]]
 
-          [:div.row
-           [:div.col-sm-6 [:h2 "Datepicker (using " [:a {:href "https://github.com/dbushell/Pikaday"} "Pikaday"] ")"]]
-           [:div.col-sm-6 [:h2 "Filepicker"]]]
+        [:div.row
+         [:div.col-sm-6 [:h2 "Datepicker (using " [:a {:href "https://github.com/dbushell/Pikaday"} "Pikaday"] ")"]]
+         [:div.col-sm-6 [:h2 "Filepicker"]]]
 
-          [:div.row
-           [f/date form "Start date" [:dates :start]
-            {:size 3
-             :min-date (t/today) :max-date end
-             :help-text "Today or later. Before end date."}]
-           [f/date form "End date" [:dates :end]
-            {:size 3
-             :empty-btn? true
-             :min-date start
-             :help-text "Optional. After start date."}]
-           [f/file form "File" [:file] {:help-text "Under 1MB"}]]
+        [:div.row
+         [f/date form "Start date" [:dates :start]
+          {:size 3
+           :min-date (t/today) :max-date end
+           :help-text "Today or later. Before end date."}]
+         [f/date form "End date" [:dates :end]
+          {:size 3
+           :min-date start
+           :help-text "Optional. After start date."}]
+         [f/file form "File" [:file] {:help-text "Under 1MB"}]]
 
-          #_
-          [:div.row
-           [:div.col-sm-12 [:h2 "Autocomplete"]]
-           [eac/country-select form "Country" [:country]]
-           [:div.form-group.col-sm-6
-            [:label "Autocomplete (tree):"]
-            [:p.form-control-static "TODO"]]]]]))))
+        #_
+        [:div.row
+         [:div.col-sm-12 [:h2 "Autocomplete"]]
+         [eac/country-select form "Country" [:country]]
+         [:div.form-group.col-sm-6
+          [:label "Autocomplete (tree):"]
+          [:p.form-control-static "TODO"]]]]])))
 
 (defonce tree-state (atom {:example-page {:lomakkeet.core/value {}
                                           :lomakkeet.core/initial-value {}
--- src/lomakkeet/reagent/datepicker.cljs
+++ src/lomakkeet/reagent/datepicker.cljs
@@ -1,14 +1,22 @@
 (ns lomakkeet.reagent.datepicker
   (:require [reagent.core :as reagent :refer [atom]]
-            [reagent.ratom :refer-macros [reaction]]
+            [reagent.ratom :refer-macros [reaction run!]]
             [goog.string :as gs]
+            [lomakkeet.core :as l]
             [lomakkeet.date :refer [date->str jsdate->local-date]]
             [lomakkeet.reagent.impl :as impl]
             cljsjs.pikaday.with-moment))
 
-(defn date* [form {:keys [ks datepicker-i18n]}]
+(defn try-deref [x]
+  (if (satisfies? IDeref x) @x x))
+
+(defn date* [form {:keys [ks datepicker-i18n min-date max-date]}]
   (let [el (atom nil)
-        value (reaction (get-in (:lomakkeet.core/value @form) ks))]
+        value (reaction (get-in (::l/value @form) ks))]
+    (if min-date
+      (run! (if @el (.setMinDate @el (try-deref min-date)))))
+    (if max-date
+      (run! (if @el (.setMaxDate @el (try-deref max-date)))))
     (reagent/create-class
       {:component-did-mount
        (fn [this]
