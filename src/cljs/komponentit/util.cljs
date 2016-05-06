0dc1338cd804345ca72b6bc71d39befcc8012105
Juho Teperi <juho.teperi@iki.fi>
2016-05-06 16:26:27 +0300

    Remove unused code

--- src/cljs/komponentit/util.cljs
+++ src/cljs/komponentit/util.cljs
@@ -1,33 +1,5 @@
 (ns komponentit.util)
 
-(defn dissoc-in
-  "Dissociates an entry from a nested associative structure returning a new
-   nested structure. keys is a sequence of keys. Any empty maps that result
-   will not be present in the new structure."
-  [m [k & ks :as keys]]
-  (if ks
-    (if-let [nextmap (get m k)]
-      (let [newmap (dissoc-in nextmap ks)]
-        (if (seq newmap)
-          (assoc m k newmap)
-          (dissoc m k)))
-      m)
-    (dissoc m k)))
-
-(defn keep-visible!
-  "Given parent element with scrollbar, keep the children visible."
-  [parent el]
-  (let [par-offset (.-scrollTop parent)
-        par-height (.-clientHeight parent)
-        child-offset (.-offsetTop el)
-        child-height (.-clientHeight el)]
-    ; Scroll down
-    (if (> (+ child-offset child-height) (+ par-offset par-height))
-      (set! (.-scrollTop parent) (- (+ child-offset child-height) par-height))
-      ; Scroll up
-      (if (< child-offset par-offset)
-        (set! (.-scrollTop parent) child-offset)))))
-
 (defn limit
   "Limit x: a < x b"
   [a b x]
