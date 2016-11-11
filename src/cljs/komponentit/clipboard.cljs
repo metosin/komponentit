e98aa52326c08de33c7e6a471816aad4afaa2fb2
Juho Teperi <juho.teperi@iki.fi>
2016-11-11 10:04:59 -0800

    Simplify copy-text

--- src/cljs/komponentit/clipboard.cljs
+++ src/cljs/komponentit/clipboard.cljs
@@ -3,14 +3,9 @@
 (defn copy-text [text]
   (let [el (js/document.createElement "textarea")
         prev-focus-el js/document.activeElement]
-    (set! (.-style el) #js {:fontSize "12pt"
-                            :border 0
-                            :padding 0
-                            :margin 0
-                            :position "absolute"
+    (set! (.-style el) #js {:position "absolute"
                             :left "-9999px"
                             :top "-9999px"})
-    (set! (.-readOnly el) "")
     (set! (.-value el) text)
     (js/document.body.appendChild el)
     (.setSelectionRange el 0 (.. el -value -length))
