33fa46a3cce92771e92dc81ced06a3f63382929c
Juho Teperi <juho.teperi@iki.fi>
2016-11-28 14:48:54 +0200

    Fix #6: Copy-text shouldn't scroll the page

--- src/cljs/komponentit/clipboard.cljs
+++ src/cljs/komponentit/clipboard.cljs
@@ -2,11 +2,20 @@
 
 (defn copy-text [text]
   (let [el (js/document.createElement "textarea")
-        prev-focus-el js/document.activeElement]
+        prev-focus-el js/document.activeElement
+        y-pos (or (.. js/window -pageYOffset)
+                  (.. js/document -documentElement -scrollTop))]
     (set! (.-style el) #js {:position "absolute"
                             :left "-9999px"
-                            :top "-9999px"})
+                            :top (str y-pos "px")
+                            ;; iOS workaround?
+                            :fontSize "12pt"
+                            ;; reset box-model
+                            :border "0"
+                            :padding "0"
+                            :margin "0"})
     (set! (.-value el) text)
+    (.addEventListener el "focus" (fn [_] (.scrollTo js/window 0 y-pos)))
     (js/document.body.appendChild el)
     (.setSelectionRange el 0 (.. el -value -length))
     (.focus el)
