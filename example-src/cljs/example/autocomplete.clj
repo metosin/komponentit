266d39f8bc2b1cdc1d824f99c82dfcbd9d6f9bc0
Juho Teperi <juho.teperi@iki.fi>
2018-12-20 14:41:20 +0200

    For some reason old macro doesn't work with new Cljs

--- example-src/cljs/example/autocomplete.clj
+++ example-src/cljs/example/autocomplete.clj
@@ -2,7 +2,7 @@
   (:require [clj-suomi.codesets.postalcodes :as postalcodes]))
 
 (defmacro get-postalcodes []
-  (into (sorted-map)
-        (map (fn [{:keys [code name]}]
-               [code (str code " " name)])
-             (postalcodes/load-postalcodes))))
+  `(into (sorted-map)
+         ~(vec (map (fn [{:keys [code name]}]
+                      [code (str code " " name)])
+                    (postalcodes/load-postalcodes)))))
